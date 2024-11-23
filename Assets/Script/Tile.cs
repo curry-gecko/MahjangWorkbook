530e0410f2dba25788875ce9630939b260619ffd
curry-gecko <curry.gecko@gmail.com>
2024-11-23 23:19:22 +0900

    牌の初期化まで

---
+++ Assets/Script/Tile.cs
@@ -0,0 +1,54 @@
+using System;
+using UnityEngine;
+
+namespace CGC.App
+{
+    [Serializable]
+    public class Tile
+    {
+        public Tile(TileSuit suit, bool isRedDora)
+        {
+            this.Suit = suit;
+            this.IsRedDora = isRedDora;
+
+        }
+        public TileSuit Suit { get; private set; }
+        public int Number { get; private set; } // 数字（1～9）: 字牌の場合はnull
+        public bool IsRedDora { get; private set; }
+
+        public Tile(TileSuit suit, int number = 0, bool isRedDora = false)
+        {
+            Suit = suit;
+            Number = number;
+            IsRedDora = isRedDora;
+        }
+
+        public override string ToString()
+        {
+            return IsRedDora ? $"{Suit} {Number}(赤)" : $"{Suit} {Number}";
+        }
+    }
+
+    public enum TileSuit
+    {
+        Manzu,
+        Pinzu,
+        Souzu,
+        Fonpai,
+        Sangenpai
+    }
+
+    public enum Fonpai
+    {
+        Ton = 1,
+        Nan = 2,
+        Sha = 3,
+        Pei = 4
+    }
+    public enum Sangenpai
+    {
+        Haku = 1,
+        Hatsu = 2,
+        Chun = 3
+    }
+}
---
+++ Assets/Script/TileManager.cs
@@ -0,0 +1,84 @@
+using System;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+namespace CGC.App
+{
+    public class TileManager : MonoBehaviour
+    {
+        [SerializeField]
+        private List<Tile> Tiles;
+
+        public bool useRedDora = false;
+
+        void Start()
+        {
+            Tiles = new List<Tile>();
+            InitializeTiles();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        // 牌の初期化
+        private void InitializeTiles()
+        {
+            Tiles.Clear();
+
+            // 数牌
+            AddNumberedTiles(TileSuit.Manzu);
+            AddNumberedTiles(TileSuit.Pinzu);
+            AddNumberedTiles(TileSuit.Souzu);
+
+            // 風牌
+            AddHonorTiles(TileSuit.Fonpai, (int)Fonpai.Ton);
+            AddHonorTiles(TileSuit.Fonpai, (int)Fonpai.Nan);
+            AddHonorTiles(TileSuit.Fonpai, (int)Fonpai.Sha);
+            AddHonorTiles(TileSuit.Fonpai, (int)Fonpai.Pei);
+
+            // 三元牌
+            AddHonorTiles(TileSuit.Sangenpai, (int)Sangenpai.Haku);
+            AddHonorTiles(TileSuit.Sangenpai, (int)Sangenpai.Hatsu);
+            AddHonorTiles(TileSuit.Sangenpai, (int)Sangenpai.Chun);
+
+            // 赤ドラ追加 TODO (省略)
+
+        }
+
+        // 数牌を追加する
+        private void AddNumberedTiles(TileSuit suit)
+        {
+            for (int n = 1; n <= 9; n++)
+            {
+                for (int i = 0; i < 4; i++)
+                {
+                    Tiles.Add(new Tile(suit, n));
+                }
+            }
+        }
+
+        // 字牌を追加する
+        private void AddHonorTiles(TileSuit suit, int number)
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                Tiles.Add(new Tile(suit, number));
+            }
+        }
+
+        public void ShuffleTiles()
+        {
+
+            System.Random rng = new();
+            for (int i = Tiles.Count - 1; i > 0; i--)
+            {
+                int swapIndex = rng.Next(i + 1);
+                (Tiles[swapIndex], Tiles[i]) = (Tiles[i], Tiles[swapIndex]);
+            }
+        }
+    }
+}
